d38c2c07358212bda059e642f526c8ad296e3b7e
euni <kongeuni1004@gmail.com>
2023-11-13 18:13:37 +0900

    refactor : MVC 패턴에 맞게 리팩토링
    내용
    - 날짜입력 로직 MVC 리펙토링

--- src/main/java/christmas/controller/InputController.java
+++ src/main/java/christmas/controller/InputController.java
@@ -8,11 +8,13 @@ public class InputController {
 
     InputService inputService = new InputService();
     Validator validator = new Validator();
+    InputView inputView = new InputView();
 
-    public int dateController(String input) {
+    public int dateController() {
 
         while (true) {
             try {
+                String input = inputView.readDate();
                 int day = validator.isNumeric(input);
                 validator.isValidDay(day);
                 return day;
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -6,12 +6,9 @@ import christmas.model.Validator;
 
 public class InputView {
 
-    InputController inputController = new InputController();
-
-    public int readDate() {
+    public String readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        String input = Console.readLine();
-        return inputController.dateController(input);
+        return Console.readLine();
     }
 
     public String readMenu(){
